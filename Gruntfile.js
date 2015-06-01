832fcf8e368ecd6030ee1d52210a5281358bceec
Anton Eprev <a.eprev@gmail.com>
2015-06-01 21:22:20 +0200

    Removed p + p

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function (grunt) {
     var isDeploy = grunt.cli.tasks.length >= 1 && grunt.cli.tasks.indexOf('deploy') > -1,
         isLiveReload = grunt.cli.tasks.length >= 1 && grunt.cli.tasks.indexOf('watch') > -1,
         version = {
-            css: 11,
+            css: 12,
             js : 6
         },
         prefix = {
