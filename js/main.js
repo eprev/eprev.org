cecf41dcde693e32ffc2a4b1f0c59ad7b3ceca5f
Anton Eprev <a.eprev@gmail.com>
2016-12-25 12:10:53 +0100

    Add webpack manifest

---
+++ js/main.js
@@ -0,0 +1,6 @@
+import './src/dom.js';
+import { Eyed } from './src/Eyed.js';
+
+new Eyed();
+
+console.log('Hello');
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,38 @@
-module.exports = {
-    entry: './js/main.js',
+const path = require('path');
+// const webpack = require('webpack');
+const BabiliPlugin = require('babili-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+
+module.exports = (options) => {
+  const plugins = [
+    new ManifestPlugin({
+      fileName: path.join('..', '_data', 'manifest.json'),
+    }),
+  ];
+  if (!options.dev) {
+    plugins.push(
+      new BabiliPlugin()
+    );
+  }
+  return {
+    devServer: {
+      contentBase: path.join(__dirname, '_site'),
+      clientLogLevel: 'none',
+      compress: true,
+      port: 4000,
+    },
+    devtool: options.dev ? 'cheap-module-eval-source-map' : 'hidden-source-map',
+    entry: {
+      bundle: './js/main.js',
+    },
     output: {
-        path: '.',
-        filename: 'main.js'
-    }
+      path: path.join(__dirname, 'assets'),
+      publicPath: 'assets/',
+      filename: '[name].[chunkhash].js',
+    },
+    performance: {
+      hints: false,
+    },
+    plugins,
+  };
 };
