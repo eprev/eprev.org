b4007abbb7d4b682d4ab0745d3bafbf44dfca145
Anton Eprev <a.eprev@gmail.com>
2017-01-29 18:58:58 +0100

    Move fontfaceobserver into separate bundle

--- js/main.js
+++ js/main.js
@@ -1,16 +1,4 @@
-import './vendor/fontfaceobserver';
 import './src/polyfills';
 import { eyed } from './src/eyed';
 
 eyed( document.querySelector('.page__user-picture') );
-
-Promise.all([
-  (new FontFaceObserver('Merriweather Sans', {weight: 300})).load(),
-  (new FontFaceObserver('Merriweather Sans', {weight: 700})).load(),
-  (new FontFaceObserver('Merriweather Sans', {weight: 300, style: 'italic'})).load(),
-  (new FontFaceObserver('Merriweather', {weight: 400})).load(),
-]).then(() => {
-  sessionStorage.fontsLoaded = 'yes';
-  document.documentElement.classList.add('fonts-loaded');
-});
-
