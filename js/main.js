4e4d4fbb85c99292c6e8f4c60574692e7f5fb9f2
Anton Eprev <a.eprev@gmail.com>
2017-01-29 11:07:43 +0100

    Add web-fonts

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
+import './vendor/fontfaceobserver';
 import './src/polyfills';
 import { eyed } from './src/eyed';
 
 eyed( document.querySelector('.page__user-picture') );
+
+Promise.all(
+  ['Merriweather Sans', 'Merriweather'].map(name => {
+    (new FontFaceObserver(name)).load()
+  })
+).then(() => {
+  document.documentElement.classList.add('fonts-loaded');
+  sessionStorage.setItem('fonts-loaded', 'yes');
+});
+
