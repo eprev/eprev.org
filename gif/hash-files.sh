2518a1c247c2b77ff6283a9c513c9d1d579a2f64
Anton Eprev <a.eprev@gmail.com>
2017-02-02 21:12:04 +0100

    Gifs

---
+++ gif/hash-files.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Hashing..."
+for f in *.gif; do
+  f_ext=${f##*.}
+  f_hash=$(md5 -q $f)
+  f_name=${f%%.*}
+  if [ "$f_hash" != "$f_name" ]; then
+    echo "  $f"
+    mv "$f" "$f_hash.$f_ext"
+  fi
+done
+
+echo "Making frames..."
+for f in *.gif; do
+  f_png=${f%%.*}.png
+  if [ ! -f "$f_png" ]; then
+    echo "  $f"
+    convert "$f[0]" "$f_png"
+  fi
+done
+
+echo "Generating markup..."
+for f in *.gif; do
+  if ! grep -Fq "$f" index.html; then
+    f_png=${f%%.*}.png
+    echo "<img src=\"/gif/$f_png\" data-src=\"/gif/$f\" alt=\"\">"
+  fi
+done
