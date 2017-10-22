a14238ddd69fe5d36b14562719f1ea1c7eaca1b4
Anton Eprev <a.eprev@gmail.com>
2017-10-22 16:12:15 +0200

    Add ga.js

---
+++ _includes/ga.js
@@ -0,0 +1,26 @@
+window.addEventListener('error', function(e) {
+  ga('send', 'event', 'JavaScript Error', e.message, e.filename ? (e.filename + ':' + e.lineno) : 'N/A', {nonInteraction: 1});
+});
+if (window.performance && document.hidden !== undefined) {
+  var prevTimeStamp = performance.now();
+  var timeOnPage = 0;
+  var accTimeOnPage = function () {
+    if (document.hidden) {
+        timeOnPage += Math.round(performance.now() - prevTimeStamp);
+    } else {
+        prevTimeStamp = performance.now();
+    }
+  };
+  document.addEventListener('visibilitychange', accTimeOnPage);
+  window.addEventListener('unload', function () {
+    accTimeOnPage();
+    ga('send', 'timing', 'Engagement', 'Time On Page', timeOnPage, {transport: 'beacon'});
+  });
+}
+document.body.addEventListener('click', (e) => {
+  if (e.target.matches('[data-ga-on=click]')) {
+    const data = e.target.dataset;
+    ga('send', 'event', data.gaCategory, data.gaAction, data.gaLabel, data.gaValue);
+  }
+});
+
--- js/src/ga.js
+++
@@ -1,6 +0,0 @@
-document.body.addEventListener('click', (e) => {
-  if (e.target.matches('[data-ga-on=click]')) {
-    const data = e.target.dataset;
-    ga('send', 'event', data.gaCategory, data.gaAction, data.gaLabel, data.gaValue);
-  }
-});
