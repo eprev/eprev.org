41b55fa3ecfe3eff4618ae80d9f66730ee76433e
Anton Eprev <a.eprev@gmail.com>
2017-10-22 17:09:31 +0200

    Deploy

--- js/src/comments.js
+++ js/src/comments.js
@@ -2,28 +2,26 @@ const plural = function (n, ...forms) {
   return forms[n === 1 ? 0 : 1];
 };
 
-if (window.fetch) {
-  const el = document.querySelector('[data-issue-id]');
-  if (el) {
-    const issueId = el.dataset.issueId;
-    fetch(`https://api.github.com/repos/eprev/eprev.org/issues/${issueId}`, {
-        headers: new Headers({
-          'Accept': 'application/vnd.github.v3.text+json'
-        })
-    }).then(response => {
-      if (response.status === 200) {
-        response.json().then(json => {
-          const comments = json.comments;
-          if (comments) {
-            const html = `There ${
-                plural(comments, 'is', 'are')
-              } <a href="https://github.com/eprev/eprev.org/issues/${ issueId }">${ comments } ${
-                plural(comments, 'comment', 'comments')
-              }</a> on this (visit the post’s issue page on GitHub)`;
-            el.innerHTML = html;
-          }
-        })
-      }
-    });
-  }
+const el = document.querySelector('[data-issue-id]');
+if (el) {
+  const issueId = el.dataset.issueId;
+  fetch(`https://api.github.com/repos/eprev/eprev.org/issues/${issueId}`, {
+      headers: new Headers({
+        'Accept': 'application/vnd.github.v3.text+json'
+      })
+  }).then(response => {
+    if (response.status === 200) {
+      response.json().then(json => {
+        const comments = json.comments;
+        if (comments) {
+          const html = `There ${
+              plural(comments, 'is', 'are')
+            } <a href="https://github.com/eprev/eprev.org/issues/${ issueId }">${ comments } ${
+              plural(comments, 'comment', 'comments')
+            }</a> on this (visit the post’s issue page on GitHub).`;
+          el.querySelector('.post-comments__text').innerHTML = html;
+        }
+      })
+    }
+  });
 }
