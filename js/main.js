f08626b354fd138e8f63b6ce49ac94b9ee49ef67
Anton Eprev <a.eprev@gmail.com>
2017-10-22 16:12:02 +0200

    Deploy

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 import eyed from './src/eyed.js';
 import './src/comments.js';
 import scrollbar from './src/scrollbar.js';
-import './src/ga.js';
 
 eyed( document.querySelector('.page__user-picture') );
 scrollbar();
