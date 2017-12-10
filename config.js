4bb796f081cc2f494590c0fbea35ead6459dabda
Anton Eprev <a.eprev@gmail.com>
2017-12-10 19:10:25 +0100

    Productinize build

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   title: "Anton Eprev",
   description: "My name’s Anton Eprev and I’m a web developer working as front-end engineer at Booking.com in Amsterdam, Netherlands",
+  url: process.env.NODE_ENV === 'production' ? 'http://eprev.org' : 'https://localhost:4000',
   twitter: "eprev",
 };
