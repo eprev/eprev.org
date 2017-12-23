e7f370f8589bd579bd572945b175ab1e2bdfcf06
Anton Eprev <a.eprev@gmail.com>
2017-12-23 20:09:47 +0100

    Deploy

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+import eyed from './src/eyed.js';
+import './src/comments.js';
+import scrollbar from './src/scrollbar.js';
+
+eyed( document.querySelector('.page__user-picture') );
+scrollbar();
---
+++ assets/js/src/comments.js
@@ -0,0 +1,27 @@
+const plural = function (n, ...forms) {
+  return forms[n === 1 ? 0 : 1];
+};
+
+const el = document.querySelector('[data-issue-id]');
+if (el) {
+  const issueId = el.dataset.issueId;
+  fetch(`https://api.github.com/repos/eprev/eprev.org/issues/${issueId}`, {
+      headers: new Headers({
+        'Accept': 'application/vnd.github.v3.text+json'
+      })
+  }).then(response => {
+    if (response.status === 200) {
+      response.json().then(json => {
+        const comments = json.comments;
+        if (comments) {
+          const html = `There ${
+              plural(comments, 'is', 'are')
+            } <a href="https://github.com/eprev/eprev.org/issues/${ issueId }">${ comments } ${
+              plural(comments, 'comment', 'comments')
+            }</a> on this (visit the post’s issue page on GitHub).`;
+          el.querySelector('.post-comments__text').innerHTML = html;
+        }
+      })
+    }
+  });
+}
