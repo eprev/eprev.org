4b42760706e769370716a29a5087122279114d67
Anton Eprev <a.eprev@gmail.com>
2017-12-25 16:54:15 +0100

    Add tags.properties

--- plugins/tags.plugin.js
+++ plugins/tags.plugin.js
@@ -1,17 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const properties = require('../lib/properties');
+
 module.exports = function({ generate, model, config }) {
-  const tagName = {
-    css: 'CSS',
-    devops: 'DevOps',
-    git: 'GIT',
-    graphql: 'GraphQL',
-    html: 'HTML',
-    javascript: 'JavaScript',
-    mongodb: 'MongoDB',
-    'os-x': 'OS X',
-    svg: 'SVG',
-    ux: 'UX',
-    'hacking-web': 'Hacking Web',
-  };
+  const tagName = properties(fs.readFileSync(path.join(__dirname, 'tags.properties'), 'utf8'));
   const tags = {};
   Object.values(model.documents).forEach(doc => {
     if (doc.type == 'post' && doc.tags) {
