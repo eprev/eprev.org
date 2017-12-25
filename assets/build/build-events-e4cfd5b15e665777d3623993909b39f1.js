a916100a118dfccb471bfa2c35c662e27c56d342
Anton Eprev <a.eprev@gmail.com>
2017-12-25 13:43:25 +0100

    Deploy

--- assets/build/build-events-e4cfd5b15e665777d3623993909b39f1.js
+++
@@ -1,1 +0,0 @@
-(function(){'use strict';var a=new EventSource('/build-events');a.onmessage=b=>{'done'===b.data&&location.reload()}})();
--- assets/js/build-events.js
+++ assets/js/build-events.js
@@ -1,6 +1,10 @@
-var source = new EventSource('/build-events');
-source.onmessage = (event) => {
-  if (event.data === 'done') {
+if (window.EventSource) {
+  var source = new EventSource('/build-events');
+  source.addEventListener('build-success', event => {
     location.reload();
-  }
-};
+  });
+  source.addEventListener('build-error', event => {
+    const error = JSON.parse(event.data);
+    console.info(error);
+  });
+}
