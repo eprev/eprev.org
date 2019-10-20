774934d74d378a4462831d4702a32aa4379a4f83
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-20 10:49:01 +0000

    Deploy eprev/eprev.org to github.com/eprev/eprev.org.git:gh-pages

--- assets-bundles/build-events-65e71c46e96a6caa6f5463795409530e.js
+++
@@ -1,1 +0,0 @@
-(function(){'use strict';if(window.EventSource){var a=new EventSource('/build-events');a.addEventListener('build-success',()=>{location.reload()}),a.addEventListener('build-error',b=>{const c=JSON.parse(b.data);console.info(c)})}})();
