0543cf10d33ef7fc8886464f518f24f4cac37d2f
Anton Eprev <a.eprev@gmail.com>
2019-11-03 17:18:08 +0000

    Advanced properties

--- wsngn/lib/properties.js
+++ wsngn/lib/properties.js
@@ -1,15 +1,32 @@
 module.exports = function properties(s, { transform = false } = {}) {
   return s && s.split(/\n+(?=\S|$)/g).reduce((acc, line) => {
     if (line) {
-      let [key, value] = line.split(/ *: */);
-      if (value.includes('\n')) {
-        const values = value.split(/\n+ +- +/);
-        if (values.length === 1) {
-          // If not a list, then concatenate lines
-          value = value.replace(/\n */g, ' ');
+      const col = line.indexOf(':');
+      let key = line.slice(0, col).replace(/ +$/, '');
+      let value = line.slice(col + 1).replace(/^ +/, '');
+      // At this point the value can be a list, a dictionary or multi-line text
+      if (value.startsWith('\n')) {
+        // Check whether the value is a dictionary
+        const propMatch = value.match(/\n+( +)\S+:/);
+        if (propMatch) {
+          const indent = propMatch[1];
+          value = properties(value.replace(new RegExp('\n' + indent, 'g'), '\n'));
         } else {
-          // Return the list
-          value = values.slice(1);
+          // At this point the value can be a list or multi-line text
+          const values = value.split(/\n+ +- +/);
+          if (values.length === 1) {
+            // If not a list, then concatenate lines
+            value = value.replace(/\n */g, ' ').replace(/^ +/, '');
+          } else {
+            // Return the list (drop the empty line)
+            value = values.slice(1);
+          }
+        }
+      } else {
+        if (value === 'true') {
+          value = true;
+        } else if (value === 'false') {
+          value = false;
         }
       }
       if (transform) {
---
+++ wsngn/lib/properties.test.js
@@ -0,0 +1,46 @@
+const describe = require('./describe');
+
+describe(`properties`, it => {
+  const parse = require('./properties');
+
+  describe(`dictionary`, it => {
+    it('treat : as key-value separator', assert => {
+      assert.deepEqual(parse('foo: bar\neof: true\nquz: 1'), {
+        foo: "bar",
+        eof: true,
+        quz: 1,
+      });
+    });
+    it('ignore spaces between key and value', assert => {
+      assert.deepEqual(parse('foo  :  bar'), {
+        foo: "bar",
+      });
+    });
+  });
+
+  describe(`multi-line values`, it => {
+    it('support multi-line text', assert => {
+      assert.deepEqual(parse('greeting:\n  hello\n  world\neof: true'), {
+        greeting: "hello world",
+        eof: true
+      });
+    });
+    it('support lists', assert => {
+      assert.deepEqual(parse('tags:\n  - foo\n  - bar\neof: true'), {
+        tags: ['foo', 'bar'],
+        eof: true
+      });
+    });
+    it('support nested structures', assert => {
+      assert.deepEqual(parse('user:\n first-name: Alice\n last-name: Cooper\n byname:\n  - Fox\n  - Wolf\neof: true'), {
+        user: {
+          'first-name': 'Alice',
+          'last-name': 'Cooper',
+          byname: ['Fox', 'Wolf']
+        },
+        eof: true
+      });
+    });
+  });
+
+});
