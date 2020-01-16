d2f7616b0feb3e99ab2dd55a25cf99360e766fc8
Anton Eprev <a.eprev@gmail.com>
2020-01-16 13:10:51 +0000

    Add goatcounter

---
+++ templates/includes/gc.js
@@ -0,0 +1,8 @@
+(function() {
+window.counter = 'https://eprev_org.goatcounter.com/count'
+var script = document.createElement('script');
+script.async = 1;
+script.src = '//gc.zgo.at/count.js';
+var ins = document.getElementsByTagName('script')[0];
+ins.parentNodee.insertBefore(script, ins)
+})();
