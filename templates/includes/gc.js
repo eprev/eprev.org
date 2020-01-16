23b17269bcaebba82f36b4657d4dea82f430ad2f
Anton Eprev <a.eprev@gmail.com>
2020-01-16 13:58:27 +0000

    Fix typo

--- templates/includes/gc.js
+++ templates/includes/gc.js
@@ -4,5 +4,5 @@ var script = document.createElement('script');
 script.async = 1;
 script.src = '//gc.zgo.at/count.js';
 var ins = document.getElementsByTagName('script')[0];
-ins.parentNodee.insertBefore(script, ins)
+ins.parentNode.insertBefore(script, ins)
 })();
